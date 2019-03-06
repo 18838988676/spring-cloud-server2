cae87fc27b426880ab2bf8ea9c538923328fd547
cec <cec@cec-PC>
2019-03-06 18:38:42 +0800

    ewew

--- src/main/java/cn/com/group/SpringCloudServer2Application.java
+++ src/main/java/cn/com/group/SpringCloudServer2Application.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class SpringCloudServer2Application {
 
 	public static void main(String[] args) {
+		//
 		SpringApplication.run(SpringCloudServer2Application.class, args);
 	}
 
