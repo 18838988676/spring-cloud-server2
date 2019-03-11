b1b3ab0f8bac2564002db07a5b8e9c3a259aa7c9
cec <cec@cec-PC>
2019-03-11 09:39:40 +0800

    统一命名服务

---
+++ src/main/java/cn/com/SpringCloudUserApplication.java
@@ -0,0 +1,13 @@
+package cn.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudUserApplication.class, args);
+	}
+
+}
