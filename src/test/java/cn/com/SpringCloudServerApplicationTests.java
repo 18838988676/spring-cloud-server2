45c047b0ef97af1cd0f1433aef139fce34fab5f3
cec <cec@cec-PC>
2019-03-11 09:49:22 +0800

    test包统一命名服务

--- src/test/java/cn/com/SpringCloudServerApplicationTests.java
+++ src/test/java/cn/com/SpringCloudServerApplicationTests.java
@@ -1,4 +1,4 @@
-package cn.com.group;
+package cn.com;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringCloudServer2ApplicationTests {
+public class SpringCloudServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
